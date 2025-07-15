2405f3e9813017fdb8b0ba47a51137308dbecffc
jegarcia <jegarcia@lwolf.com>
2025-07-15 13:02:14 -0500

    last commit

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import './style.css'
 //import './clean-code/07-tarea';
 //import './code-smells/02-open-close-a';
 //import './code-smells/03-liskov-a';
-import './solid/07-segregation';
+// import './solid/01-segregation';
+import './solid/05-dependency-a';
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
